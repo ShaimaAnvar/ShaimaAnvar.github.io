06d95657f1af22fc6f326d1ac0aac368e0a73d7f
Shaima <shaima.anvar@gmail.com>
2021-08-11 16:32:37 +0530

    form submission completed

--- personal website template_files/script.js
+++ personal website template_files/script.js
@@ -16,6 +16,20 @@ $(document).ready(function(){
             message:{
                 required:true,
             }
+        },
+        submitHandler: function(form) {
+            $.ajax({
+                url:"https://script.google.com/macros/s/AKfycbyTgCJwxs4KXG1oof9y8MWF_-nRrvJ4OZglABeg9QqjIFexfWVcKtEHpDs27b324DV4aA/exec",
+                data:$("#signup-form").serialize(),
+                method:"post",
+                success:function(response){
+                    alert("Form submitted successfully")
+                    window.location.reload()
+                },
+                error:function (err){
+                    alert("Something Error")
+                }
+            })
         }
     })
 })
